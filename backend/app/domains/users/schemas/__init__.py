ea521b0ce92f5a70f4edd3baf77859572c360446
pedpalma <pedro.francisco.palma@gmail.com>
2026-07-20 16:27:14 -0300

    chore(users): added __init__ to schemas

---
+++ backend/app/domains/users/schemas/__init__.py
@@ -0,0 +1,8 @@
+from app.domains.users.schemas.users import (
+    RoleType,
+    UserCreate,
+    UserResponse,
+    UserUpdate,
+)
+
+__all__ = ["RoleType", "UserCreate", "UserResponse", "UserUpdate"]
