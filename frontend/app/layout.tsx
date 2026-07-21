87ecd4ebd399701ee7ed609f1aad5cd846c1332b
pedpalma <pedro.francisco.palma@gmail.com>
2026-07-21 15:44:07 -0300

    feat(app): added authprovider to layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { AuthProvider } from "../components/private/AuthProvider";
 
 export const metadata: Metadata = {
   title: "Status G-Lab Telecom",
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" className="h-full antialiased">
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col font-sans">
+        <AuthProvider>{children}</AuthProvider>
+      </body>
     </html>
   );
 }
